7d9ec1d0f72354453e6892565e3080e10a6a7dae
Yevhen Havrysh <39010905+27ugeen@users.noreply.github.com>
2024-10-02 10:19:52 +0300

    hw done

---
+++ src/api/apiImageService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const fetchImages = async (query, page) => {
+  try {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query, page, per_page: 12 },
+      headers: {
+        Authorization: `Client-ID ${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`,
+      },
+    });
+    return response.data.results;
+  } catch (error) {
+    console.log(`error: ${error.message}`);
+    throw new Error("Failed to fetch images");
+  }
+};
